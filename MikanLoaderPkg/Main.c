11ba8586e0612eba114c56cc1c61d1026a272c57
0x6d61 <0x6d61@gmail.com>
2021-04-22 23:13:55 +0900

    最初のkernelを作成

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -146,6 +146,33 @@ EFI_STATUS EFIAPI UefiMain(
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
 
+    EFI_FILE_PROTOCOL* kernel_file;
+
+    root_dir->Open(
+        root_dir,&kernel_file,L"\\kernel\\kernel.elf",
+        EFI_FILE_MODE_READ,0
+    );
+
+    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+    UINT8 file_into_buffer[file_info_size];
+    kernel_file->GetInfo(
+        kernel_file,&gEfiFileInfoGuid,
+        &file_info_size,file_info_buffer);
+    )
+
+    EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+    UINTN kernel_file_size = file_info->FileSize;
+
+    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+
+    gBS->AllocatePages(
+        AllocateAddress,EfiLoaderData,
+        (kernel_file_size + 0xfff) / 0x1000,&kernel_base_addr
+    );
+
+    kernel_file->Read(kernel_file,&kernel_file_size,(VOID*)kernel_base_addr);
+    Print(L"Kernel: 0x%0lx (%lu bytes)\n",kernel_base_addr,kernel_file_size);
+
     Print(L"All done\n");
     while (1)
         ;
