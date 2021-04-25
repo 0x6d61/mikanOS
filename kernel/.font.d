d30437f533df31a265b29db283a64b95356e21aa
0x6d61 <0x6d61@gmail.com>
2021-04-25 17:31:33 +0900

    ファイルを分割

---
+++ kernel/.font.d
@@ -0,0 +1,5 @@
+font.o: font.cpp font.hpp \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
