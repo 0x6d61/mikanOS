c8a52c36138901405cbd60ed9197f6d535e38fe4
0x6d61 <0x6d61@gmail.com>
2021-04-25 17:34:14 +0900

    .*.d 削除

--- kernel/.graphics.d
+++
@@ -1,3 +0,0 @@
-graphics.o: graphics.cpp graphics.hpp frame_buffer_config.hpp \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__config
--- kernel/.main.d
+++
@@ -1,7 +0,0 @@
-main.o: main.cpp \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__config \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
-  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/version \
-  frame_buffer_config.hpp graphics.hpp font.hpp
