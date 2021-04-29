bc04e2bfddc4e8e2e7335fc6b2c932de4429f6a7
0x6d61 <0x6d61@gmail.com>
2021-04-29 19:16:55 +0900

    マウスが動いたやったね！

---
+++ kernel/usb/.memory.d
@@ -0,0 +1,6 @@
+usb/memory.o: usb/memory.cpp usb/memory.hpp \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h
---
+++ kernel/usb/classdriver/.keyboard.d
@@ -0,0 +1,39 @@
+usb/classdriver/keyboard.o: usb/classdriver/keyboard.cpp usb/classdriver/keyboard.hpp \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/functional \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/type_traits \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/typeinfo \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/exception \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstdlib \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdlib.h \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/memory \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/new \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/utility \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__tuple \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/initializer_list \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstring \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/string.h \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__debug \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/limits \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/iterator \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/iosfwd \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/wchar.h \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/__functional_base \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/tuple \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdexcept \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cassert \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/assert.h \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/_ansi.h \
+  usb/classdriver/hid.hpp usb/classdriver/base.hpp error.hpp \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/array \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/algorithm \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/bit \
+  usb/endpoint.hpp usb/setupdata.hpp usb/memory.hpp usb/device.hpp \
+  usb/arraymap.hpp \
+  /home/x6d61/osbook/devenv/x86_64-elf/include/c++/v1/optional
